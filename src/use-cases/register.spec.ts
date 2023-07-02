c1891ef8f39e69efa788dc034777dd709bcd422b
fabio-c266 <fabiocaldas266@gmail.com>
2023-07-02 11:18:48 -0300

    fix: import wrong

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,6 +1,6 @@
 import { it, describe, expect, beforeEach } from 'vitest'
 import { RegisterUseCase } from './register'
-import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { compare } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists'
 
